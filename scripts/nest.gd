5e34efe502004ea8cba476bd7733c992ca6bfd0e
Waltteri Suomi <waltteri.suomi@gmail.com>
2024-01-24 16:43:23 +0200

    added nest to spawn bugs based on pollution level

--- scripts/nest.gd
+++ scripts/nest.gd
@@ -3,9 +3,22 @@ extends Node
 @onready var main = $".."
 @onready var enemies: Dictionary = $"..".enemies
 @onready var units: Dictionary = $"..".units
+@onready var spawn_timer = $SpawnTimer
 
 var rng = RandomNumberGenerator.new()
 var spawn_range = 2
+var state = States.Normal
+
+enum SpawnCooldowns {Normal, LowPollution, MediumPollution, HighPollution, Alert}
+enum States {Normal, Alert}
+
+var spawn_cooldowns = {
+	SpawnCooldowns.Normal:60,
+	SpawnCooldowns.LowPollution:45,
+	SpawnCooldowns.MediumPollution:35,
+	SpawnCooldowns.HighPollution:25,
+	SpawnCooldowns.Alert:15,
+}
 
 func _on_timer_timeout():
 	try_spawn_random()
@@ -17,3 +30,19 @@ func try_spawn_random():
 	if tile_pos.x >= 0 and tile_pos.x < main.GRID_WIDTH and tile_pos.y >= 0 and tile_pos.y < main.GRID_HEIGHT:
 		if tile_pos not in enemies and tile_pos not in units and main.is_walkable(main.tiles[tile_pos]):
 			main.spawn_enemy(main.Enemies.Bug,pos + offset)
+
+func change_spawn_cooldown(cooldown):
+	print("Spawn Cooldown: ", spawn_cooldowns[cooldown])
+	spawn_timer.wait_time = spawn_cooldowns[cooldown]
+
+func _on_check_timer_timeout():
+	var tile = main.tiles[main.map.local_to_map(self.global_position)]
+	if state == States.Alert:
+		return
+	print(tile.pollution)
+	if tile.pollution > 1 and tile.pollution <= 10:
+		change_spawn_cooldown(SpawnCooldowns.LowPollution)
+	if tile.pollution > 10 and tile.pollution <= 20:
+		change_spawn_cooldown(SpawnCooldowns.MediumPollution)
+	if tile.pollution > 20:
+		change_spawn_cooldown(SpawnCooldowns.HighPollution)
